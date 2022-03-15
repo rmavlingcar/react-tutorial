4ca3f338f7b795278adb9c708e08f4a2e196833f
Ricky M <rickymavlingcar@live.com>
2022-03-15 15:54:47 -0500

    Completed tutorial #12

--- react-tutorial-blog/src/Home.js
+++ react-tutorial-blog/src/Home.js
@@ -5,11 +5,14 @@ const Home = () => {
     const [blogs, setBlogs] = useState([
         { title: 'My new website', body: 'lorem ipsum...', author: 'Ricky', id: 1 },
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'Robin', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Shaun', id: 3 }
+        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Ricky', id: 3 },
+        { title: 'Automation of aggregating 2D images online with python', body: 'lorem ipsum...', author: 'Shaun', id: 4 },
+        { title: 'Why Miss Fortune is the only champion you should play', body: 'lorem ipsum...', author: 'Joe', id: 5 }
     ])
     return ( 
     <div className="home">
         <BlogList blogs={ blogs } title="All Blogs"></BlogList>
+        <BlogList blogs={ blogs.filter((blog) => blog.author === 'Ricky')} title="Ricky's Blogs"></BlogList>
     </div> 
     );
 }
