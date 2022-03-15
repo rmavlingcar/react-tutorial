3216a80593b4da352ff84e852e103fc0e3fd6a86
Ricky M <rickymavlingcar@live.com>
2022-03-15 14:51:21 -0500

    Completed tutorial #7

--- react-tutorial-blog/src/Home.js
+++ react-tutorial-blog/src/Home.js
@@ -1,7 +1,20 @@
 const Home = () => {
+
+    const handleClick = (e) => {
+        console.log('Hello World!', e);
+    }
+
+    const handleClickAgain = (name, e) => {
+        console.log('Hello, '+name, e.target);
+    }
+
     return ( 
     <div className="home">
         <h2>Homepage</h2>
+        <button onClick={handleClick}>Click Me</button>
+        <button onClick={(e) => {
+            handleClickAgain('mario',e)
+        }}>Click Me Again</button>
     </div> 
     );
 }
