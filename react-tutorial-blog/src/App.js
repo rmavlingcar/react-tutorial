47e7e0094230018f3b45a59f31d9b8430d9e6391
Ricky M <rickymavlingcar@live.com>
2022-03-16 16:37:21 -0500

    Completed tutorial #21

--- react-tutorial-blog/src/App.js
+++ react-tutorial-blog/src/App.js
@@ -1,16 +1,23 @@
 
 import Navbar from './Navbar';
 import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   
   return (
-    <div className="App">
-      <Navbar />
-      <div className="content">
-        <Home></Home>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Switch>
+            <Route path='/'>
+              <Home></Home>
+            </Route>
+          </Switch>
+        </div>
       </div>
-    </div>
+    </Router>
   );
 }
 
