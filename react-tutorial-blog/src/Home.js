39e908a5b3a5288cb7783f082c00c6fefe385082
Ricky M <rickymavlingcar@live.com>
2022-03-16 10:47:11 -0500

    Completed tutorial #18

--- react-tutorial-blog/src/Home.js
+++ react-tutorial-blog/src/Home.js
@@ -3,14 +3,19 @@ import BlogList from './BlogList';
 
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
+    const[isPending, setIsPending] = useState(true);
     
     useEffect(() => {
         //console.log("useEffect hook triggered!");
-        fetch('http://localhost:8000/blogs').then(res => { return res.json(); }).then(data => { setBlogs(data); });
+        fetch('http://localhost:8000/blogs').then(res => { return res.json(); }).then(data => { 
+            setBlogs(data);
+            setIsPending(false)
+        });
     }, []);
 
     return ( 
     <div className="home">
+        { isPending && <div>Loading...</div>}
         {blogs && <BlogList blogs={ blogs } title="All Blogs"></BlogList>}
     </div> 
     );
